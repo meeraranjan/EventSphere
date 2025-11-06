c1a9e98b153be01b5567526b8daac4ea9d7ee04f
meeraranjan <153428812+meeraranjan@users.noreply.github.com>
2025-11-06 04:12:42 -0500

    added link in django admin ui to access dashboard

--- eventsphere/admin_dashboard/admin.py
+++ eventsphere/admin_dashboard/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
+from django.shortcuts import redirect
 
-# Register your models here.
+# Add a title with dashboard link
+admin.site.site_header = "EventSphere Admin"
+admin.site.site_title = "EventSphere Admin"
+admin.site.index_title = format_html(
+    'Welcome to EventSphere Admin | <a href="{}">📊 View Dashboard</a>',
+    reverse('admin_dashboard')
+)
