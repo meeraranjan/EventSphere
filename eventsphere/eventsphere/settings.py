c5433eda66b13e4cdc5087da9cffa9a0dcc28fb8
meeraranjan <153428812+meeraranjan@users.noreply.github.com>
2025-11-06 03:57:51 -0500

    sending email notifications upon rsvp updates

--- eventsphere/eventsphere/settings.py
+++ eventsphere/eventsphere/settings.py
@@ -135,3 +135,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 GOOGLE_MAPS_API_KEY = 'AIzaSyCAZGcPey98efdwODbN7bumoJp8ybon9ds'
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "eventspherecompany.info@gmail.com"
+EMAIL_HOST_PASSWORD = "iopm xghv kabx qtci"
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
