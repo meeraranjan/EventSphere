6413c28c3caf9b177586e748bf9c9ac639d8fe81
Meera Ranjan <meeraranjan.2004@gmail.com>
2025-12-06 04:44:23 +0000

    website fixes

--- eventsphere/eventsphere/settings.py
+++ eventsphere/eventsphere/settings.py
@@ -144,7 +144,7 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "eventspherecompany.info@gmail.com"
-EMAIL_HOST_PASSWORD = "iopm xghv kabx qtci"
+EMAIL_HOST_PASSWORD = "iopmxghvkabxqtci"
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
